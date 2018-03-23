7b87ca594934455251a183b63ed1377c61f59f8c
xiaomian <812083426@qq.com>
2018-03-23 16:44:30 +0800

    每日提交

--- src/main/java/com/thinkgem/jeesite/modules/hm/web/doc/ZdocController.java
+++ src/main/java/com/thinkgem/jeesite/modules/hm/web/doc/ZdocController.java
@@ -4,7 +4,8 @@
 package com.thinkgem.jeesite.modules.hm.web.doc;
 
 import java.io.File;
-import java.io.UnsupportedEncodingException;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,6 +20,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.aspose.words.Document;
+import com.aspose.words.License;
 import com.thinkgem.jeesite.common.config.Global;
 import com.thinkgem.jeesite.common.persistence.Page;
 import com.thinkgem.jeesite.common.utils.CacheUtils;
@@ -27,7 +30,6 @@ import com.thinkgem.jeesite.common.web.BaseController;
 import com.thinkgem.jeesite.modules.hm.entity.doc.Zdoc;
 import com.thinkgem.jeesite.modules.hm.service.doc.ZdocService;
 import com.thinkgem.jeesite.modules.hm.utils.ZdocUtils;
-import com.thinkgem.jeesite.modules.hm.utils.offeic.filetools.DocChangePdf;
 
 /**
  * 文档中心Controller
@@ -90,43 +92,83 @@ public class ZdocController extends BaseController {
 				String filetype = string.substring(string.lastIndexOf(".") + 1, string.length());
 				System.out.println("name:------>"+name);
 				System.out.println("filetype:------>"+filetype);
-
 				
 				String path = string.substring(1, string.lastIndexOf("/"));
 				System.out.println("path:------>"+path);
-				try {
-					name = java.net.URLDecoder.decode(name, "utf-8");
-				} catch (UnsupportedEncodingException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				String dfile = path + name + "." + filetype;		//当前文件 service下的路径	
-				String filepath = request.getSession().getServletContext().getRealPath("/") + dfile; 		//当前文件的绝对路径		
+				// 验证License
 				
-				File file = new File(filepath);
-				System.out.println(filepath+"------------->file"+ file.exists());
-				String savefilepath = request.getSession().getServletContext().getRealPath("/");
-				if ("docx".equals(filetype) || "doc".equals(filetype) || "xls".equals(filetype)
-						|| "xlsx".equals(filetype) || "ppt".equals(filetype) || "pptx".equals(filetype)) {
-					System.out.println("goto doc");
-					String filepathname = DocChangePdf.changefiles(savefilepath + path, file, name + "." + filetype);	
-					System.out.println("----------");
-					System.out.println(savefilepath);
-					System.out.println(filepathname);
-//					//最后得到的pdf
-					String sile = filepathname.substring(savefilepath.length()-1, filepathname.length());
-					System.out.println(sile);
-					
-					sile = sile.replaceAll("\\\\", "/");
-					zdoc.setFiles(sile);
-				}
+				String dfile = path + name + "." + filetype;		//当前文件 service下的路径
+				String filepath = request.getSession().getServletContext().getRealPath("/") + dfile; //当前文件的绝对路径				
+				
+				
+				String chulifile = path + name + ".pdf"; 
+				String filepaths = request.getSession().getServletContext().getRealPath("/") + chulifile;  //转换后文件的绝对路径				
+				
+				
+		        if (!getLicense(request)) {
+		            return form(zdoc, model);
+		        }
+		        try {
+		            long old = System.currentTimeMillis();	
+		            
+		            System.out.println(filepath + "------------------------>filepath");
+		            Document doc = new Document(filepath);		      
+		            
+		            System.out.println(filepaths + "------------------------>filepaths");
+		            doc.save(filepaths);
+		            long now = System.currentTimeMillis();
+		            zdoc.setFiles(chulifile);
+		            System.out.println("共耗时：" + ((now - old) / 1000.0) + "秒");
+		        } catch (Exception e) {
+		            e.printStackTrace();
+		        }
+//				try {
+//					name = java.net.URLDecoder.decode(name, "utf-8");
+//				} catch (UnsupportedEncodingException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+//				String dfile = path + name + "." + filetype;		//当前文件 service下的路径	
+//				String filepath = request.getSession().getServletContext().getRealPath("/") + dfile; 		//当前文件的绝对路径		
+//				
+//				File file = new File(filepath);
+//				System.out.println(filepath+"------------->file"+ file.exists());
+//				String savefilepath = request.getSession().getServletContext().getRealPath("/");
+//				if ("docx".equals(filetype) || "doc".equals(filetype) || "xls".equals(filetype)
+//						|| "xlsx".equals(filetype) || "ppt".equals(filetype) || "pptx".equals(filetype)) {
+//					System.out.println("goto doc");
+//					String filepathname = DocChangePdf.changefiles(savefilepath + path, file, name + "." + filetype);	
+//					System.out.println("----------");
+//					System.out.println(savefilepath);
+//					System.out.println(filepathname);
+////					//最后得到的pdf
+//					String sile = filepathname.substring(savefilepath.length()-1, filepathname.length());
+//					System.out.println(sile);
+//					
+//					sile = sile.replaceAll("\\\\", "/");
+//					zdoc.setFiles(sile);
+//				}
 			}			
 		}
-		
 		zdocService.save(zdoc);
 		addMessage(redirectAttributes, "保存文档成功");
 		return "redirect:"+Global.getAdminPath()+"/hm/doc/zdoc/?repage";
 	}
+	
+	 public static boolean getLicense(HttpServletRequest request) {
+        boolean result = false;
+        try {
+        	File file = new File(request.getSession().getServletContext().getRealPath("/") + "WEB-INF/classes/licenseword.xml");
+        	InputStream is = new FileInputStream(file);
+//            InputStream is = ZdocController.class.getClassLoader().getResourceAsStream("\\licenseword.xml");
+            License aposeLic = new License();
+            aposeLic.setLicense(is);
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 	
 	@RequiresPermissions("hm:doc:zdoc:edit")
 	@RequestMapping(value = "delete")
